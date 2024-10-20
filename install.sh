211ffd3565b062d14afd58e025074dd1f1c50bc7
dev.corvo <097.cuervo@gmail.com>
2024-10-19 21:34:36 -0300

    Demo - Population Analysis

---
+++ install.sh
@@ -0,0 +1,4 @@
+python -m venv davenv
+source davenv/bin/activate
+pip install -r requirements.txt
+pre-commit install
--- main.py
+++ main.py
@@ -25,7 +25,3 @@ def plot():
     
     return send_file(img, mimetype='image/png')
 
-
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = '8000', debug = True)
-
